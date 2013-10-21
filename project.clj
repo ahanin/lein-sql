4acbe237faf055ee021f00f61beb56956ea3b00d
Alexey Hanin <alexey.hanin@gmail.com>
2013-10-21 23:15:05 +0200

    removed unwanted dependency

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.com/ahanin/lein-sql"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[leinjacker "0.4.2"]]
+  :dependencies []
   :profiles {:dev {:plugins [[lein-midje "3.1.1"]]
                    :dependencies [[midje "1.5.1"]]}}
   :eval-in-leiningen true)
