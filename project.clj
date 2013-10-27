c9ffe29b432ee56a8b9a59313b2326766de9feca
Alexey Hanin <alexey.hanin@gmail.com>
2013-10-27 18:31:25 +0100

    attempt to fix Travis CI Midje problem, trying postgres service

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://github.com/ahanin/lein-sql"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[leinjacker "0.4.2"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [leinjacker "0.4.2"]
                  [org.clojure/java.jdbc "0.3.0-alpha5"]]
   :profiles {:dev {:dependencies [[midje "1.5.1" :exclusions [org.clojure/clojure]]]
                    :plugins [[lein-midje "3.1.1"]]}}
