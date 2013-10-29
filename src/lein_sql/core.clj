ba61d9f664eaa5e94330b61c673b5a6c6272761f
Alexey Hanin <alexey.hanin@gmail.com>
2013-10-29 23:05:51 +0100

    naive directory scanning regexp

--- src/lein_sql/core.clj
+++ src/lein_sql/core.clj
@@ -1,6 +1,7 @@
 (ns lein-sql.core
   (:import [java.sql DriverManager])
-  (:require [clojure.java.jdbc :as jdbc]))
+  (:require [clojure.java.jdbc :as jdbc])
+  (:require [clojure.string :as string]))
 
 (defmacro lein-sql-command [command & control-map]
   `(~(symbol (str "lein-sql.core/" command)) ~@control-map))
@@ -18,6 +19,42 @@
         (DriverManager/getConnection connection-uri)
       :else (throw (IllegalArgumentException. (format "%s missing a required parameter" opts))))))
 
+(def ^:dynamic *db-conn*)
+
+(defn execute-query [query]
+  (with-open [s (.createStatement *db-conn*)]
+     (.execute s query)))
+
+(defn re-quote [s]
+  (let [special (set ".?*+^$[]\\(){}|")
+    escfn #(if (special %) (str \\ %) %)]
+      (apply str (map escfn s))))
+
+(defn path-pattern-string [path-pattern]
+  (str "^"
+    (string/join
+      (into []
+        (for [x (re-seq #"(?:[\*]+/?|[^\*]+)" path-pattern)]
+          (cond
+            (re-matches #"\*{2,}/?" x) ".*"
+            (re-matches #"\*" x) ".*"
+            :else (re-quote x))
+          )))
+    "$"))
+
+(defn path-pattern [path-pattern]
+  (re-pattern (path-pattern-string path-pattern)))
+
+(defn execute-files [file-pattern]
+  (let [path-regexp (file-pattern)]
+    (doseq [f (file-seq file-pattern)]
+      (println f))))
+
+(defmacro with-connection [db-spec & body]
+  `(with-open [conn# (get-connection ~db-spec)]
+     (binding [*db-conn* conn#]
+       ~@body)))
+
 (defn execute
   "Execute SQL query
 
@@ -29,14 +66,15 @@
   :password         Password
 
   :query            SQL query to execute
+  :files            Files from which to sequentially read and execute SQL statements
 
   1. Using JDBC url
   lein sql :query \"CREATE DATABASE my_db\" :connection-uri jdbc:postgresql://localhost/ :driver org.postgresql.Driver
   "
   [project args]
-  (when-let [q (get-query args)]
-    (with-open [c (get-connection args)]
-       (with-open [s (.createStatement c)]
-         (.execute s (:query args))
-         ))))
+  (let [{:keys [query files]} args]
+    (cond query (with-connection args (execute-query query))
+          files (with-connection args (execute-files files))
+          :else (throw (IllegalArgumentException. (format "%s missing a required argument"))
+                       ))))
 
---
+++ test/lein_sql/t_core.clj
@@ -0,0 +1,14 @@
+(ns lein-sql.t-core
+  (:use midje.sweet)
+  (:require [lein-sql.core :as core]))
+
+(fact "path-pattern-string transforms a path pattern to a regexp string"
+  (core/path-pattern-string "**/*.sql") => "^.*.*\\.sql$"
+    )
+
+(fact "path-pattern build a regexp that matches path string"
+  (re-matches (core/path-pattern "**/*.sql") "a/b/c/file.sql") => "a/b/c/file.sql"
+  (re-matches (core/path-pattern "a/**/*.sql") "a/b/c/file.sql") => "a/b/c/file.sql"
+  (re-matches (core/path-pattern "**/*.sql") "file.sql") => "file.sql"
+  (re-matches (core/path-pattern "**/b/*.sql") "a/b/c/file.sql") => "a/b/c/file.sql"
+    )
